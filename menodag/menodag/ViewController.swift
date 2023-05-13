089b48a5f2d8f7f9c3cefa84f84af0981e3d35cb
O Rabie <o.rabie@tap.company>
2023-05-13 19:11:46 +0300

    Splash screen

---
+++ menodag/menodag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  menodag
+//
+//  Created by Osama Rabie on 13/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
